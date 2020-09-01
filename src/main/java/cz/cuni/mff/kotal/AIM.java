44eb7439643a0177d9b8083c824891794a41635b
PC <jirikotal@vivaldi.net>
2020-09-01 13:13:30 +0200

    Implemented basic JavaFX.

--- src/main/java/cz/cuni/mff/kotal/AIM.java
+++ src/main/java/cz/cuni/mff/kotal/AIM.java
@@ -1,7 +1,15 @@
 package cz.cuni.mff.kotal;
 
+import cz.cuni.mff.kotal.frontend.MainApp;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class AIM {
    public static void main(String[] args) {
       System.out.println("Hello there.");
+      Application.launch(MainApp.class, args);
    }
 }
