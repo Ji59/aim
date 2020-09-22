d1779b5f68e0d40948edce80f1eb9e4242f2e4c2
PC <jirikotal@vivaldi.net>
2020-09-22 17:32:08 +0200

    Created abstract model for square and octagonal.

--- src/main/java/cz/cuni/mff/kotal/frontend/intersection/IntersectionGraph.java
+++ src/main/java/cz/cuni/mff/kotal/frontend/intersection/IntersectionGraph.java
@@ -2,12 +2,14 @@ package cz.cuni.mff.kotal.frontend.intersection;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
 public class IntersectionGraph extends Pane {
-   private static final double PADDING = 30;
-   private static final double OCTAGON_RATIO = 1 / Math.E;
+   private static final double PADDING = 30, GOLDEN_RATIO = (Math.sqrt(5) - 1) / 2;
+   private static final double OCTAGON_RATIO = 1 / Math.E, VERTEX_RATIO = GOLDEN_RATIO / 2;
    private static final Color ROAD_COLOR = Color.LIGHTGREY,
       STROKE_COLOR = Color.BLACK,
       ENTRY_COLOR = Color.LIGHTSLATEGRAY,
@@ -29,35 +31,62 @@ public class IntersectionGraph extends Pane {
 
       drawBackground(height);
 
+      // TODO add real / abstract model button
+
       drawSquareEntriesAndExits(granularity, entries, exits, shift);
 
       for (int i = 1; i <= granularity; i++) {
          for (int j = 1; j <= granularity; j++) {
-            Rectangle square = new Rectangle(shift, shift, ROAD_COLOR);
-            square.setX(i * shift + PADDING);
-            square.setY(j * shift + PADDING);
-            square.setStroke(STROKE_COLOR);
-            getChildren().add(square);
+//            drawSquareRoad(shift, i * shift + PADDING, j * shift + PADDING);
+            drawAbstractSquareRoad(shift, (i + 0.5) * shift + PADDING, (j + 0.5) * shift + PADDING, i < granularity, j < granularity);
          }
       }
    }
 
+   private void drawSquareRoad(double shift, double x, double y) {
+      Rectangle square = new Rectangle(shift, shift, ROAD_COLOR);
+      square.setX(x);
+      square.setY(y);
+      square.setStroke(STROKE_COLOR);
+      getChildren().add(square);
+   }
+
+   private void drawAbstractSquareRoad(double shift, double x, double y, boolean withRightRoad, boolean withDownRoad) {
+      if (withRightRoad) {
+         Line rightRoad = new Line(x, y, x + shift, y);
+         getChildren().add(rightRoad);
+      }
+
+      if (withDownRoad) {
+         Line downRoad = new Line(x, y, x, y + shift);
+         getChildren().add(downRoad);
+      }
+
+      Circle vertex = new Circle(x, y, shift * VERTEX_RATIO);
+      vertex.setFill(ROAD_COLOR);
+      vertex.setStroke(STROKE_COLOR);
+
+      getChildren().add(vertex);
+   }
+
    private void drawSquareEntriesAndExits(long granularity, long entries, long exits, double shift) {
 
-      // TODO udelej to poradne!
+      // TODO add real / abstract model button
 
       long empty = granularity - entries - exits,
          padding = empty / 3,
          index = empty % 3 == 2 ? ++padding : padding;
 
       while (entries-- > 0) {
-         createRectangularEntries(granularity, shift, ++index, true, true);
+//         drawRectangularEntries(granularity, shift, ++index, true, true);
+         drawAbstractRectangularEntries(granularity, shift, ++index, true);
       }
 
       index += empty - 2 * padding;
 
       while (exits-- > 0) {
-         createRectangularEntries(granularity, shift, ++index, false, true);
+//         drawRectangularEntries(granularity, shift, ++index, false, true);
+         drawAbstractRectangularEntries(granularity, shift, ++index, false);
       }
    }
 
@@ -74,7 +103,7 @@ public class IntersectionGraph extends Pane {
          double x = sidePadding,
             y = (granularity / 2. + i) * shift + PADDING;
          for (int j = 0; j < i + granularity; j++, x += edgeLength * 1.5, y -= shift / 2) {
-            createHexagon(shift, edgeLength, x, y);
+            drawHexagon(shift, edgeLength, x, y);
          }
       }
 
@@ -82,7 +111,7 @@ public class IntersectionGraph extends Pane {
          double x = sidePadding + 1.5 * i * edgeLength,
             y = (granularity * 3 + i - 2) * shift / 2 + PADDING;
          for (int j = 0; j < granularity * 2 - i - 1; j++, x += edgeLength * 1.5, y -= shift / 2) {
-            createHexagon(shift, edgeLength, x, y);
+            drawHexagon(shift, edgeLength, x, y);
          }
       }
 
@@ -228,7 +257,7 @@ public class IntersectionGraph extends Pane {
       }
    }
 
-   private void createHexagon(double shift, double edgeLength, double x, double y) {
+   private void drawHexagon(double shift, double edgeLength, double x, double y) {
       Polygon hexagon = new Polygon(
          x, y,                                  // top left
          x + edgeLength, y,                                      // top right
@@ -252,64 +281,89 @@ public class IntersectionGraph extends Pane {
 
       drawBackground(height);
 
-      drawOctagonalLine(granularity, 1, shift, false, false);
-
-      for (int i = 2; i < granularity; i++) {
-         drawOctagonalLine(granularity, i, shift, false, true);
-      }
-
-      drawOctagonalLine(granularity, granularity, shift, true, false);
-
       drawOctagonalEntriesAndExits(granularity, entries, exits, shift);
+
+      for (int i = 1; i <= granularity; i++) {
+         drawOctagonalLine(granularity, i, shift, i < granularity, i < granularity && i != 1);
+      }
    }
 
-   private void drawOctagonalLine(long granularity, long row, double shift, boolean notSquares, boolean withCorners) {
-      double topY = row * shift + PADDING,
-         midTopY = (row + OCTAGON_RATIO) * shift + PADDING;
+   private void drawOctagonalLine(long granularity, long row, double shift, boolean withSquares, boolean withCorners) {
+      double topY = row * shift + PADDING;
+
+      // TODO add real / abstract model button
 
       for (int j = withCorners ? 1 : 2; j <= (withCorners ? granularity : granularity - 1); j++) {
-         createOctagonRoute(shift, topY, midTopY, j);
+//         drawOctagonRoad(shift, topY, (row + OCTAGON_RATIO) * shift + PADDING, j);
+         drawAbstractOctagonRoad(shift, (j + 0.5) * shift + PADDING, topY + shift / 2, granularity, row, j);
       }
 
-      if (notSquares) {
+      if (!withSquares) {
          return;
       }
 
       for (int j = 2; j <= granularity; j++) {
-         Polygon square = new Polygon(
-            j * shift + PADDING, midTopY + (1 - 2* OCTAGON_RATIO) * shift,      // top
-            (j - OCTAGON_RATIO) * shift + PADDING, topY + shift,                                      // right
-            j * shift + PADDING, midTopY + shift,                                                                     // bottom
-            (j + OCTAGON_RATIO) * shift + PADDING, topY + shift                                       // left
-         );
-
-         square.setFill(ROAD_COLOR);
-         square.setStroke(STROKE_COLOR);
-
-         getChildren().add(square);
+//         drawOctagonalSquareRoad(shift, topY, (row + OCTAGON_RATIO) * shift + PADDING, j);
+         drawAbstractOctagonalSquareRoad(shift, j * shift + PADDING, topY + shift, row < granularity - 1 || j > 2, row < granularity - 1 || j < granularity);
       }
+   }
+
+   private void drawOctagonalSquareRoad(double shift, double topY, double midTopY, int j) {
+      Polygon square = new Polygon(
+         j * shift + PADDING, midTopY + (1 - 2 * OCTAGON_RATIO) * shift,      // top
+         (j - OCTAGON_RATIO) * shift + PADDING, topY + shift,                                      // right
+         j * shift + PADDING, midTopY + shift,                                                                     // bottom
+         (j + OCTAGON_RATIO) * shift + PADDING, topY + shift                                       // left
+      );
+
+      square.setFill(ROAD_COLOR);
+      square.setStroke(STROKE_COLOR);
+
+      getChildren().add(square);
+   }
+
+   private void drawAbstractOctagonalSquareRoad(double shift, double x, double y, boolean withLeftLine, boolean withRightLine) {
+      if (withLeftLine) {
+         Line leftObliqueLine = new Line(x, y, x - shift / 2, y + shift / 2);
+
+         getChildren().add(leftObliqueLine);
+      }
+
+      if (withRightLine) {
+         Line rightObliqueLine = new Line(x, y, x + shift / 2, y + shift / 2);
+
+         getChildren().add(rightObliqueLine);
+      }
+
+      Circle vertex = new Circle(x, y, shift * VERTEX_RATIO);
+      vertex.setFill(ROAD_COLOR);
+      vertex.setStroke(STROKE_COLOR);
+
+      getChildren().add(vertex);
    }
 
    private void drawOctagonalEntriesAndExits(long granularity, long entries, long exits, double shift) {
 
-      // TODO udelej to poradne!
+      // TODO add real / abstract model button
 
       long empty = granularity - entries - exits,
          padding = empty / 3,
          index = empty % 3 == 2 ? ++padding : padding;
 
       while (entries-- > 0) {
-         createRectangularEntries(granularity, shift, ++index, true, false);
+//         drawRectangularEntries(granularity, shift, ++index, true, false);
+         drawAbstractRectangularEntries(granularity, shift, ++index, true);
       }
 
       index += empty - 2 * padding;
 
       while (exits-- > 0) {
-         createRectangularEntries(granularity, shift,  ++index, false, false);
+//         drawRectangularEntries(granularity, shift, ++index, false, false);
+         drawAbstractRectangularEntries(granularity, shift, ++index, false);
       }
    }
 
-   private void createRectangularEntries(long granularity, double shift, long index, boolean entry, boolean square) {
+   private void drawRectangularEntries(long granularity, double shift, long index, boolean entry, boolean square) {
       double side2 = shift * (1 + (square ? 0 : OCTAGON_RATIO)),
          endPadding = (granularity + 1 - (square ? 0 : OCTAGON_RATIO)) * shift + PADDING;
 
@@ -339,7 +393,41 @@ public class IntersectionGraph extends Pane {
       getChildren().addAll(entryN, entryS, entryW, entryE);
    }
 
-   private void createOctagonRoute(double shift, double topY, double midTopY, int j) {
+   private void drawAbstractRectangularEntries(long granularity, double shift, long index, boolean entry) {
+      double length = shift * (1 + OCTAGON_RATIO),
+         centerNX = (index + 0.5) * shift + PADDING,
+         centerNY = shift * 0.5 + PADDING,
+         centerSX = (granularity - index + 1.5) * shift + PADDING,
+         centerSY = (granularity + 1 + 0.5) * shift + PADDING;
+
+      Line entryNLine = new Line(centerNX, centerNY, centerNX, centerNY + shift),
+         entrySLine = new Line(centerSX, centerSY - shift, centerSX, centerSY),
+         entryWLine = new Line(centerNY, centerSX, centerNY + shift, centerSX),
+         entryELine = new Line(centerSY - shift, centerNX, centerSY, centerNX);
+
+      Color color = entry ? ENTRY_COLOR : EXIT_COLOR;
+
+      Circle entryNVertex = new Circle(centerNX, centerNY, shift * VERTEX_RATIO),
+         entrySVertex = new Circle(centerSX, centerSY, shift * VERTEX_RATIO),
+         entryWVertex = new Circle(centerNY, centerSX, shift * VERTEX_RATIO),
+         entryEVertex = new Circle(centerSY, centerNX, shift * VERTEX_RATIO);
+
+      entryNVertex.setFill(color);
+      entryNVertex.setStroke(STROKE_COLOR);
+
+      entrySVertex.setFill(color);
+      entrySVertex.setStroke(STROKE_COLOR);
+
+      entryWVertex.setFill(color);
+      entryWVertex.setStroke(STROKE_COLOR);
+
+      entryEVertex.setFill(color);
+      entryEVertex.setStroke(STROKE_COLOR);
+
+      getChildren().addAll(entryNLine, entrySLine, entryWLine, entryELine, entryNVertex, entrySVertex, entryWVertex, entryEVertex);
+   }
+
+   private void drawOctagonRoad(double shift, double topY, double midTopY, int j) {
       Polygon octagon = new Polygon(
          (j + OCTAGON_RATIO) * shift + PADDING, topY,                        // top left
          (j + 1 - OCTAGON_RATIO) * shift + PADDING, topY,                                   // top right
@@ -355,6 +443,25 @@ public class IntersectionGraph extends Pane {
       octagon.setStroke(STROKE_COLOR);
 
       getChildren().add(octagon);
+   }
+
+   private void drawAbstractOctagonRoad(double shift, double x, double y, long granularity, long row, long column) {
+      boolean withRightRoad = (row > 1 && row < granularity && column < granularity) || column < granularity - 1,
+         withDownRoad = row < granularity - 1 || (row == granularity - 1 && column > 1 && column < granularity);
+
+      if (row < granularity && column > 1) {
+         Line obliqueLeftEdge = new Line(x, y, x - shift / 2, y + shift / 2);
+
+         getChildren().add(obliqueLeftEdge);
+      }
+
+      if (row < granularity && column < granularity) {
+         Line obliqueRightEdge = new Line(x, y, x + shift / 2, y + shift / 2);
+
+         getChildren().add(obliqueRightEdge);
+      }
+
+      drawAbstractSquareRoad(shift, x, y, withRightRoad, withDownRoad);
    }
 
    private void drawBackground(double height) {
