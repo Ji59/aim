b04d352d4648981dab813bb548c71cb3b5cd450f
jiri <jirikotal@vivaldi.net>
2021-09-05 21:27:51 +0200

    Completed AgentPaneTest rotation test.

--- src/test/java/cz/cuni/mff/kotal/frontend/simulation/AgentPaneTest.java
+++ src/test/java/cz/cuni/mff/kotal/frontend/simulation/AgentPaneTest.java
@@ -34,14 +34,23 @@ class AgentPaneTest {
 		assert cornerPoints.contains(new Point(CENTER_X - HALF_WIDTH, CENTER_Y + HALF_LENGTH));
 		assert cornerPoints.contains(new Point(CENTER_X + HALF_WIDTH, CENTER_Y + HALF_LENGTH));
 
-		double newAngle = Math.PI / 4;
-		checkCornersAtAngle(newAngle);
+		checkCornersAtAngle2(100, 100);
+		checkCornersAtAngle2(100, 50);
+		checkCornersAtAngle2(70, 100);
+		checkCornersAtAngle2(10, 300);
+		checkCornersAtAngle2(90, 100);
+		checkCornersAtAngle2(1000, 50);
+		checkCornersAtAngle2(77, 1000);
+		checkCornersAtAngle2(13.5, 42.73);
 
-		newAngle = Math.PI / 6;
-		checkCornersAtAngle(newAngle);
-
-		newAngle = Math.PI / 2;
-		checkCornersAtAngle(newAngle);
+//		double newAngle = Math.PI / 4;1
+//		checkCornersAtAngle(newAngle);
+//
+//		newAngle = Math.PI / 6;
+//		checkCornersAtAngle(newAngle);
+//
+//		newAngle = Math.PI / 2;
+//		checkCornersAtAngle(newAngle);
 	}
 
 	private void checkCornersAtAngle(double newAngle) {
@@ -81,13 +90,69 @@ class AgentPaneTest {
 		System.out.println("y = " + aCD + " * x + " + (newCY - aCD * newCX));
 		System.out.println("y = " + aDA + " * x + " + (newAY - aDA * newAX));
 
-//		assert cornerPointsContainPoint(cornerPoints, newAX, newAY);
-//		assert cornerPointsContainPoint(cornerPoints, newBX, newBY);
-//		assert cornerPointsContainPoint(cornerPoints, newCX, newCY);
-//		assert cornerPointsContainPoint(cornerPoints, newDX, newDY);
+		assert cornerPointsContainPoint(cornerPoints, newAX, newAY);
+		assert cornerPointsContainPoint(cornerPoints, newBX, newBY);
+		assert cornerPointsContainPoint(cornerPoints, newCX, newCY);
+		assert cornerPointsContainPoint(cornerPoints, newDX, newDY);
+	}
+
+	private void checkCornersAtAngle2(double length, double width) {
+		int centerX = 200;
+		int centerY = 100;
+		Agent agent = new Agent(0, 0, 1, 1, 0, length, width, centerX, centerY);
+
+		AgentPane agentPane = new AgentPane(agent);
+		agentPane.setLayoutX(centerX);
+		agentPane.setLayoutY(centerY);
+
+		double diagonalAngle = Math.atan(width / length);
+		double halfDiagonal = Math.sqrt(length * length + width * width) / 2;
+
+		agentPane.setAngle(diagonalAngle);
+
+		List<Point> cornerPoints;
+
+		cornerPoints = agentPane.getCornerPoints();
+		cornerPoints.forEach(point -> System.out.println(point.getX() + "; " + point.getY()));
+
+		double xShift = halfDiagonal * Math.sin(2 * diagonalAngle);
+		double yShift = halfDiagonal * Math.cos(2 * diagonalAngle);
+
+		double newAX = centerX;
+		double newAY = centerY + halfDiagonal;
+
+		double newBX = centerX - xShift;
+		double newBY = centerY + yShift;
+
+		double newCX = newAX;
+		double newCY = centerY - halfDiagonal;
+
+		double newDX = centerX + xShift;
+		double newDY = centerY - yShift;
+
+		System.out.println();
+		System.out.println(newAX + "; " + newAY);
+		System.out.println(newBX + "; " + newBY);
+		System.out.println(newCX + "; " + newCY);
+		System.out.println(newDX + "; " + newDY);
+
+		double aAB = (newBY - newAY) / (newBX - newAX);
+		double aBC = (newBY - newCY) / (newBX - newCX);
+		double aCD = (newDY - newCY) / (newDX - newCX);
+		double aDA = (newDY - newAY) / (newDX - newAX);
+
+		System.out.println("y = " + aAB + " * x + " + (newAY - aAB * newAX));
+		System.out.println("y = " + aBC + " * x + " + (newCY - aBC * newCX));
+		System.out.println("y = " + aCD + " * x + " + (newCY - aCD * newCX));
+		System.out.println("y = " + aDA + " * x + " + (newAY - aDA * newAX));
+
+		assert cornerPointsContainPoint(cornerPoints, newAX, newAY);
+		assert cornerPointsContainPoint(cornerPoints, newBX, newBY);
+		assert cornerPointsContainPoint(cornerPoints, newCX, newCY);
+		assert cornerPointsContainPoint(cornerPoints, newDX, newDY);
 	}
 
 	private boolean cornerPointsContainPoint(List<Point> cornerPoints, double newAX, double newAY) {
-		return cornerPoints.stream().anyMatch(point -> MyNumberOperations.doubleAlmostEqual(point.getX(), newAX, 1e-4) && MyNumberOperations.doubleAlmostEqual(point.getY(), newAY, 1e-4));
+		return cornerPoints.stream().anyMatch(point -> MyNumberOperations.doubleAlmostEqual(point.getX(), newAX, 1e-7) && MyNumberOperations.doubleAlmostEqual(point.getY(), newAY, 1e-7));
 	}
 }
