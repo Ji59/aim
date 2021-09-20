7ada7b6e1f9def503ab43ae00715c134137991d0
jiri <jirikotal@vivaldi.net>
2021-09-20 09:04:20 +0200

    Refactored algorithm classes.

--- src/main/java/cz/cuni/mff/kotal/backend/algorithm/Algorithm.java
+++ src/main/java/cz/cuni/mff/kotal/backend/algorithm/Algorithm.java
@@ -7,7 +7,19 @@ import java.util.Set;
 
 
 public interface Algorithm {
-	void planAgents(Set<Agent> agents);
 
+	/**
+	 * Try to plan all provided agents using an algorithm.
+	 *
+	 * @param agents Set of agents to be planned
+	 * @return Set of agents which had been successfully planned
+	 */
+	Set<Agent> planAgents(Set<Agent> agents);
+
+	/**
+	 * Try to plan agent with an algorithm.
+	 * @param agent Agent to be planned
+	 * @return Agent if planning was successful, otherwise null
+	 */
 	Agent planAgent(Agent agent);
 }
--- src/main/java/cz/cuni/mff/kotal/backend/algorithm/BreadthFirstSearch.java
+++ src/main/java/cz/cuni/mff/kotal/backend/algorithm/BreadthFirstSearch.java
@@ -12,85 +12,162 @@ import java.util.stream.Collectors;
 
 
 public class BreadthFirstSearch implements Algorithm {
-	private final Set<VertexWithVisit> vertices;
+	private final Map<Long, VertexWithVisit> vertices;
 
+	/**
+	 * Create new instance working with graph from provided simulation.
+	 *
+	 * @param simulation
+	 */
 	public BreadthFirstSearch(Simulation simulation) {
-		this.vertices = simulation.getIntersectionGraph().getVertices()
-			.stream()
-			.map(vertex -> new VertexWithVisit(vertex.getID(), vertex.getType(), vertex.getNeighbourIDs()))
-			.collect(Collectors.toSet());
+		this(simulation.getIntersectionGraph());
 	}
 
+	/**
+	 * Create new instance working with provided graph.
+	 *
+	 * @param graph Graph to search on
+	 */
 	public BreadthFirstSearch(Graph graph) {
-		this.vertices = graph.getVertices()
+		vertices = graph.getVertices()
 			.stream()
 			.map(vertex -> new VertexWithVisit(vertex.getID(), vertex.getType(), vertex.getNeighbourIDs()))
-			.collect(Collectors.toSet());
+			.collect(Collectors.toMap(Vertex::getID, Function.identity()));
 	}
 
+	/**
+	 * Plan all agents using BFS without any collision check.
+	 *
+	 * @param agents Set of agents to be planned
+	 * @return Set of successfully planned agents
+	 */
 	@Override
-	public void planAgents(Set<Agent> agents) {
-		agents.forEach(this::planAgent);
+	public Set<Agent> planAgents(Set<Agent> agents) {
+		return agents.stream().filter(agent -> planAgent(agent) != null).collect(Collectors.toSet());
 	}
 
+	/**
+	 * Plan agent using BFS on saved graph.
+	 *
+	 * @param agent Agent to be planned
+	 * @return Agent if successfully planned otherwise null
+	 */
 	@Override
 	public Agent planAgent(Agent agent) {
-		agent.setPath(bfs(agent.getStart(), agent.getEnd()));
+		try {
+			agent.setPath(bfs(agent.getStart(), agent.getEnd()));
+		} catch (Exception e) {
+			return null;
+		}
 		return agent;
 	}
 
+	/**
+	 * Perform BFS on `this.graph` searching path from start to end.
+	 *
+	 * @param startID ID of starting vertex
+	 * @param endID   ID of ending vertex
+	 * @return Found path as list
+	 * @throws RuntimeException If no path was found.
+	 */
 	private List<Long> bfs(long startID, long endID) {
-		Map<Long, VertexWithVisit> vertices = this.vertices.stream().map(vertex -> new VertexWithVisit(vertex.getID(), vertex.getType(), vertex.getNeighbourIDs())).collect(Collectors.toMap(Vertex::getID, Function.identity()));
 		VertexWithVisit start = vertices.get(startID);
+
+		// TODO add excepion
 		assert (start != null);
+
 		start.setPathAndAddSelf(new ArrayList<>());
 		Queue<VertexWithVisit> queue = new ArrayDeque<>();
 		queue.add(start);
-		start.setVisited(true);
+		start.setVisited();
 
 		while (!queue.isEmpty()) {
 			VertexWithVisit first = queue.poll();
 			if (first.getNeighbourIDs().contains(endID)) {
 				List<Long> path = first.getPath();
 				path.add(endID);
+
+				resetVisitedVertices();
 				return path;
 			}
 			first.getNeighbourIDs()
 				.stream()
 				.map(vertices::get).filter(v -> !v.isVisited()).forEach(neighbour -> {
-				neighbour.setPathAndAddSelf(new ArrayList<>(first.getPath()));
-				queue.add(neighbour);
-				neighbour.setVisited(true);
-			});
+					neighbour.setPathAndAddSelf(new ArrayList<>(first.getPath()));
+					queue.add(neighbour);
+					neighbour.setVisited();
+				});
 		}
 		// TODO create exception
-		return null;
+		resetVisitedVertices();
+		throw new RuntimeException("Path not found");
 	}
 
+	private void resetVisitedVertices() {
+		vertices.values().forEach(VertexWithVisit::setNotVisited);
+	}
+
+	/**
+	 * Vertex with saved path to it.
+	 */
 	private static class VertexWithVisit extends Vertex {
 		private boolean visited = false;
 		private List<Long> path;
 
+		/**
+		 * Create new vertex with provided parameters.
+		 *
+		 * @param id           ID of the vertex
+		 * @param type         Type of the vertex
+		 * @param neighbourIDs Set of neighbour vertices of the vertex
+		 */
 		public VertexWithVisit(long id, Type type, Set<Long> neighbourIDs) {
 			super(id, type, neighbourIDs);
 		}
 
+		/**
+		 * Create new vertex with provided parameters.
+		 *
+		 * @param id   ID of the vertex
+		 * @param type Type of the vertex
+		 */
 		public VertexWithVisit(long id, Type type) {
 			super(id, type);
 		}
 
+		/**
+		 * @return True if this vertex was visited on path otherwise False
+		 */
 		public boolean isVisited() {
 			return visited;
 		}
 
-		public void setVisited(boolean visited) {
-			this.visited = visited;
+		/**
+		 * Set visited to True.
+		 */
+		public void setVisited() {
+			visited = true;
 		}
 
+		/**
+		 * Set visited to False to reset state.
+		 */
+		public void setNotVisited() {
+			visited = false;
+		}
+
+		/**
+		 * @return Path to this vertex
+		 */
 		public List<Long> getPath() {
 			return path;
 		}
 
+		/**
+		 * Set path to this vertex to provided one and add itself in the end.
+		 *
+		 * @param path Path to append itself to
+		 */
 		public void setPathAndAddSelf(List<Long> path) {
 			this.path = path;
 			path.add(id);
