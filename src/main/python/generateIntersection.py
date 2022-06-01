da173bb0dd0b6d95ab7bb86dd1632fec78c6637a
ji59 <jirikotal@vivaldi.net>
2022-06-01 11:30:40 +0200

    Change language in plots to czech

--- src/main/python/generateIntersection.py
+++ src/main/python/generateIntersection.py
@@ -34,7 +34,6 @@ def create_graph_plot(graph: dict[str, Any], intersection: nx.DiGraph, locations
 	granularity = graph.get("granularity")
 	fig, ax = plt.subplots(1, 2, figsize=(11, 6))
 	type_ = graph.get("type")
-	fig.suptitle(type_)
 	fig.subplots_adjust(top=0.88)
 	plt.subplots_adjust(wspace=0.02, left=0.01, right=0.99, bottom=0.01, top=0.9)
 	# Set the x and y limits for each axis, leaving a 0.05 margin te ensure
@@ -45,15 +44,17 @@ def create_graph_plot(graph: dict[str, Any], intersection: nx.DiGraph, locations
 	ax[0].set_ylim([min_lim, max_lim])
 	ax[1].set_xlim([min_lim, max_lim])
 	ax[1].set_ylim([min_lim, max_lim])
-	ax[0].set_title("Graph representation")
-	ax[1].set_title("Real intersection representation")
+	ax[0].set_title("Grafová reprezentace")
+	ax[1].set_title("Vzhled reálné křižovatky")
 
 	colors = [ENTRY_COLOR if id_ in entries else EXIT_COLOR if id_ in exits else ROAD_COLOR for id_ in intersection.nodes]
 	nx.draw_networkx(intersection, locations, node_color=colors, node_shape='o', labels={}, label="Graph", ax=ax[0])
 	if type_ == "Square grid":
+		name = "Čtvercový typ"
 		cell_size = 1 / (granularity + 2)
 		nx.draw_networkx_nodes(intersection, locations, node_size=17000 * cell_size, node_color=colors, node_shape='s', label="Real", ax=ax[1])
 	elif type_ == "Octagonal grid":
+		name = "Oktagonální typ"
 		cell_size = 1 / (granularity + 2)
 		octagonal_end = granularity * granularity - 4
 		octagonal_vertices = np.arange(octagonal_end)
@@ -65,6 +66,7 @@ def create_graph_plot(graph: dict[str, Any], intersection: nx.DiGraph, locations
 		diamond_vertices = np.arange(entries_exits_end, len(locations))
 		nx.draw_networkx_nodes(intersection, locations, nodelist=diamond_vertices, node_size=4096 * cell_size, node_color=ROAD_COLOR, node_shape='D', label="Real", ax=ax[1])
 	elif type_ == "Hexagonal grid":
+		name = "Hexagonální typ"
 		cell_size = 1. / (2 * granularity + 1)
 		hexagonal_end = granularity * (granularity - 1) / 2 * 6 + 1
 		hexagonal_vertices = np.arange(hexagonal_end)
@@ -73,8 +75,9 @@ def create_graph_plot(graph: dict[str, Any], intersection: nx.DiGraph, locations
 		entries_exits_colors = [ENTRY_COLOR if id_ in entries else EXIT_COLOR for id_ in intersection.nodes if id_ in entries + exits]
 		nx.draw_networkx_nodes(intersection, locations, nodelist=entries_exits_vertices, node_size=8192 * cell_size, node_color=entries_exits_colors, node_shape='o', label="Real", ax=ax[1])
 
-	plt.savefig(type_ + ".pdf", format="pdf")
-
+	fig.suptitle(name)
+	plt.savefig(type_.replace(' ', '_') + ".pdf", format="pdf")
+	# plt.show()
 
 if __name__ == "__main__":
 	import sys
